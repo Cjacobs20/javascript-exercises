f85e6336056b1e5edb212fa669adb3c389bca3f3
Chris Jacobs <christianejacobs2020@gmail.com>
2022-12-09 12:40:53 -0700

    First two excersises completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR'
+    let inputWord = '';
+    for (let i=0; i < num; i++){
+        inputWord += string;
+    }
+    return inputWord;
 };
 
 // Do not edit below this line
